33a520175db6edb6825cf9cfc6ba29bb1081c695
Rochi Albani <rocio.albani@hotmail.com>
2023-02-19 08:09:18 -0300

    added router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Sidebar from "./components/Sidebar";
+import Header from "./components/Header";
 
 function App() {
   
-
   return (
-    <div>
-      App
+    <div className="min-h-screen grid grid-cols-1 xl:grid-cols-6">
+      <Sidebar/>
+      <div className="xl:col-span-5">
+      <Header/>
+    <Routes>
+      <Route exact path="/" element={<Home />}/>
+    </Routes>
+    </div>
     </div>
   )
 }
